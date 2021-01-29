ab5557db3031ae3701221de062878c8a1cd2506c
Kevin Balz <kevin.balz@web.de>
2021-01-29 22:31:55 +0100

    use better template

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import Phaser from 'phaser';
+import logoImg from './assets/logo.png';
+
+class MyGame extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+    }
+
+    preload ()
+    {
+        this.load.image('logo', logoImg);
+    }
+      
+    create ()
+    {
+        const logo = this.add.image(400, 150, 'logo');
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: MyGame
+};
+
+const game = new Phaser.Game(config);
