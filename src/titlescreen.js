edafd817b022f8ef14366d6f5abf36473e19f9a4
alexandra.skogseide@akamu.de <alexandra.skogseide@akamu.de>
2021-01-31 15:46:47 +0100

    titlescreen title

--- src/titlescreen.js
+++ src/titlescreen.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import backgroundImage from './assets/title_screen.png';
+import titleImage from './assets/doggolantis.png';
 
 
 class TitleScene extends Phaser.Scene{
@@ -9,16 +10,22 @@ class TitleScene extends Phaser.Scene{
 
     preload(){
         this.load.image('title_screen', backgroundImage);
+        this.load.image('doggolantis', titleImage);
     }
 
     create(){
         var background = this.add.image(0, 0, 'title_screen');
         background.setOrigin(0, 0);
 
+        var title = this. add.image(60, 70, 'doggolantis');
+        title.scale = title.scale * 0.85;
+        title.setOrigin(0, 0);
+
         //var startButton = new startButton(MyGame, 100, 100, )
-        const startButton = this.add.text(100, 100, 'Start Game', { fill: '#fff' });
+        const startButton = this.add.text(60, 480, 'Start Game', { fill: '#fff' });
         startButton.setInteractive()
                     .on('pointerdown', () => this.scene.start('main'));
+        startButton.scale = startButton.scale * 2.3;
     }
 }
 
